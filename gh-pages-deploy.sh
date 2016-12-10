a6d106469529c2a1355ceee20f8446021be00cb6
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2016-12-10 17:49:17 +0100

    Added githup-pages deployging scripts

---
+++ gh-pages-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -z "$1" ]
+then
+  echo "Please enter commit message"
+  exit 1
+fi
+
+PROJECT_SUBDIR=react-calendar-app npm run build
+
+cp ./gh-pages/404.html ./build/404.html
+
+git add build
+git commit -am '$1' --no-verify
+
+git subtree push --prefix build origin gh-pages
---
+++ src/helpers/history.js
@@ -0,0 +1,10 @@
+import {useBasename} from 'history';
+
+// This helper is for setting basename on history object.
+export function withBasename(history) {
+  /* eslint-disable no-undef */
+  const basename = (typeof __BASENAME__ !== 'undefined') ? __BASENAME__ : '';
+  console.log('basename', basename);
+  /* eslint-enable no-undef */
+  return useBasename(() => history)({basename: `/${basename}`});
+}
